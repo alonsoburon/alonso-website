a89bf4a6460f536827a789d0c48874f89ea92016
Alonso Buron Ardiles <alonso.buron@proton.me>
2025-10-04 17:04:55 -0300

    Fix CSS for custom domain - remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alonsoburon.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/alonso-website' : '/',
+  site: 'https://alonsoburon.cl',
+  base: '/',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
